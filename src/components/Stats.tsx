5e720c3d560c33efdd0192503839bc3d16f6b875
Nan Montaño <nano.mv112@gmail.com>
2021-03-26 21:00:14 -0700

    stats layout

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -3,6 +3,7 @@ import { netWpm, rawWpm } from '../util/handlers';
 import { tuplify } from '../util/std';
 import { getChars, getWords, getExtra } from '../util/text';
 import { useTyper } from './Typer';
+import '../styles/Stats.css';
 
 function Stats() {
   const state = useTyper();
@@ -13,12 +14,29 @@ function Stats() {
   }, [stats, content]);
   const counts = charCounts(content.text, typed);
   return (
-    <>
-      <p title="net/raw">Wpm: {Math.round(netWpm(state))}/{Math.round(rawWpm(state))}</p>
-      <p>Time: {stats.wpm.length}s </p>
-      <p>Acc: {Math.round(acc)}%</p>
-      <p title="correct/incorrect/extra/missing">Chars: {counts.correct}/{counts.incorrect}/{counts.extra}/{counts.missing}</p>
-    </>
+    <div className="Stats">
+      <div className="wpm">
+        <h3>wpm</h3>
+        <h2>{Math.round(netWpm(state))}</h2>
+      </div>
+      <div className="acc">
+        <h3>acc</h3>
+        <h2>{Math.round(acc)}%</h2>
+      </div>
+      <div className="chart"/>
+      <div className="raw">
+        <h3>raw</h3>
+        <h2>{Math.round(rawWpm(state))}</h2>
+      </div>
+      <div className="chars">
+        <h3>characters</h3>
+        <h2>{counts.correct}/{counts.incorrect}/{counts.extra}/{counts.missing}</h2>
+      </div>
+      <div className="time">
+        <h3>time</h3>
+        <h2>{stats.wpm.length}s</h2>
+      </div>
+    </div>
   )
 }
 
